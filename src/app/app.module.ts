ab1e67532ea76de6e323386b59db80fcdb297edb
Alexius Lim <Alexius.Lim@rpsgroup.com>
2021-09-02 13:20:32 +0800

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { DatePickerModule } from '@syncfusion/ej2-angular-calendars';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    DatePickerModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
